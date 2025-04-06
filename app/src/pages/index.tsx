3197afcb434f8e56c583729b05764ba9b0bdeda3
leejay <leejay@gemvaxlink.com>
2025-04-06 23:09:06 +0900

    vercel test commit

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
       <div className="flex flex-col items-center justify-center h-screen">
       <main className="flex flex-col text-9xl">
-        <span >Aquarium</span>
+        <span>Aquarium</span>
         <span>Archive</span>
       </main>
       </div>
