0210d8fb458f0ba33e3b64d0e4d8c9f17d304928
leejay <leejay@gemvaxlink.com>
2025-04-06 23:31:33 +0900

    add prettier & gsap

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,10 +1,9 @@
+import MainText from '@/components/MainText'
+
 export default function Home() {
   return (
-      <div className="flex flex-col items-center justify-center h-screen">
-      <main className="flex flex-col text-9xl">
-        <span>Aquarium</span>
-        <span>Archive .</span>
-      </main>
-      </div>
-  );
+    <div className="flex flex-col items-center justify-center h-screen">
+      <MainText />
+    </div>
+  )
 }
