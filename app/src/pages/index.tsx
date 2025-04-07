f818f9dbd3daed352efb55b4099e96e8088a6bdc
leejay210 <leejay@ponylink.co.kr>
2025-04-07 11:23:03 +0900

    delete

--- app/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import MainText from '@/components/MainText'
-
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <MainText />
-    </div>
-  )
-}
